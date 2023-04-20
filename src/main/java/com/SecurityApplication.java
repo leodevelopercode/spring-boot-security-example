c5d51d6541378ce9414c1316d80c4a7806afc041
leocode <nixgnixeix@gmail.com>
2023-04-20 18:56:17 +0800

    基础框架

---
+++ src/main/java/com/SecurityApplication.java
@@ -0,0 +1,17 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Xiexingxie
+ * @date 2023/4/20 下午 06:50
+ */
+@SpringBootApplication
+public class SecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication.class,args);
+    }
+    
+}
---
+++ src/test/java/com/SecurityApplicationTest.java
@@ -0,0 +1,17 @@
+package com;
+
+import org.junit.Test;
+
+/**
+ * @author Xiexingxie
+ * @date 2023/4/20 下午 06:53
+ */
+
+public class SecurityApplicationTest {
+
+    @Test
+    public void securityapplicaitontest() {
+        
+    }    
+    
+}
